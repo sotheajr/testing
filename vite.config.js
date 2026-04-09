eb03d543e00960b395fa64fc774cd15de1e9d578
Sothea Chhum <sothechhum2agmail.com>
2026-04-09 19:06:29 +0700

    fix assets

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-
-    build: {
-        outDir: "public/build", // 🔥 សំខាន់
-        emptyOutDir: true,
-    },
-
-    server: {
-        watch: {
-            ignored: ["**/storage/framework/views/**"],
-        },
-    },
 });
